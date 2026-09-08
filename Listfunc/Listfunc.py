6952d33a29a619a98ee881d79e7e7cf72b8ce3a5
Rana Shazil <ranashazil611@gmail.com>
2026-09-09 03:20:23 +0500

    add student data and display functionality

--- Listfunc/Listfunc.py
+++ Listfunc/Listfunc.py
@@ -1,3 +1,6 @@
-std_data=[{
-    {}
-}]
+std_data=[
+    {"name":"Shazil","age":20,"roll":31},
+    {"name":"ali","age":22,"roll":32},
+    {"name":"ahemd","age":21,"roll":33}]
+for std in std_data:
+ print(std["name"]," has age ",std["age"]," with roll number ",std["roll"])
