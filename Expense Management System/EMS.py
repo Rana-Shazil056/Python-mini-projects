619f3d444bdc1d92597330817c975a0be1715457
Rana Shazil <ranashazil611@gmail.com>
2026-09-18 02:38:40 +0500

    Refactor expense management functions and improve input validation

--- Expense Management System/EMS.py
+++ Expense Management System/EMS.py
@@ -1,7 +1,7 @@
 #It is an Expenses Management System
 import json
 import os
-import time
+from datetime import date
 
 DATA_FILE=os.path.join(os.path.dirname(__file__),"EMS_DATA2026.json")
 
@@ -9,9 +9,9 @@ def load_expense():
  try:
   with open(DATA_FILE,"r",encoding="utf-8") as File:
    data=json.load(File) 
-   return data if isinstance(list,data) else []
- except (FileNotFoundError,json.JSONDecodeError)
- return 
+   return data if isinstance(data,list) else []
+ except (FileNotFoundError,json.JSONDecodeError):
+  return []
 
 def save_expenses():
  with open(DATA_FILE,"w",encoding="utf-8") as file:
@@ -20,11 +20,49 @@ def save_expenses():
 
 expenses=load_expense()
 
+def add_expense():
+ try:
+  id=int(input("Enter the id for item: "))
+ except ValueError:
+  print("Invalid input! Try Again")
+  return
+ for expense in expenses:
+  if expense["ID"]==id:
+   print("OOPSS!! This id already exists")
+   return
+descr=input("Enter decription for expense: ")
+amount=float(input("Enter the amount for expense: "))
+tdate=date.today().strftime("%Y-%m-%d")
+expense_data={
+  "ID":id,
+  "Descr":descr,
+  "Amount":amount,
+  "Date":tdate
+ }
+expenses.append(expense_data)
+save_expenses()
 
+def Search():
+  try:
+   d=int(input("Enter the id to search its details: "))
+  except ValueError:
+   print("Invalid input try again")
+   return
+  for expense in expenses:
 
+   if d==expense['ID']:
+    print("\nExpenses record:\n") 
+    print(f"ID : {expense['ID']} \nDescription : {expense['Descr']} \nAmount : {expense['Amount']} \nDate : {expense['Date']}")
 
+  print("Searched Id not Found!!!")  
 
-
+def display():
+ if not expenses:
+  print("Record is empty! Nothing to Display!!!")
+ else: 
+  for expense in expenses:
+   print(f"ID : {expense['ID']} \nDescription : {expense['Descr']} \nAmount : {expense['Amount']} \nDate : {expense['Date']}")
+  
 
 def main():
  print("############    Expense Management System   ##############")
@@ -35,13 +73,17 @@ def main():
 5. Summary & Total Spent
 6. Filter by Category
 7. Exit Program''')
-# while True:
-#  try:
-#   n=int(input("Enter Your Choice:"))
-#  except ValueError:
-#   print("Enter valid choice: ")
+ while True:
+  try:
+   n=int(input("Enter Your Choice:"))
+  except ValueError:
+   print("Enter valid choice: ")
 
-#   match n:
-#    case 1:
-#     add_expense()
-# main()
+match n:
+ case 1:
+  add_expense()
+ case 2:
+  display()
+ case 3:
+     Search()  
+main()
